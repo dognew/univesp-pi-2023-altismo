f29cd80f97e518a9c47cb9315cf1167261b100da
Douglas Fiedler <dognew@gmail.com>
2023-05-31 22:19:53 -0300

    Add articles pages files

--- mysite/pagehtml/views.py
+++ mysite/pagehtml/views.py
@@ -9,3 +9,21 @@ def index(request):
 
 def contato(request):
     return render(request, 'contato.html')
+
+def art(request):
+    return render(request, 'art.html')
+
+def artb(request):
+    return render(request, 'artb.html')
+
+def artc(request):
+    return render(request, 'artc.html')
+
+def artd(request):
+    return render(request, 'artd.html')
+
+def arte(request):
+    return render(request, 'arte.html')
+
+def artf(request):
+    return render(request, 'artf.html')
